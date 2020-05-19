29b0f38a1af8140f44d7793b8b8fcd4eda348545
Kirill Mager <kiruha24@mail.ru>
2020-05-19 22:32:49 +0300

    refactor: change logic and design

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import axios from 'axios';
-import Loader from './Loader';
 
 function App() {
-  const [advice, setAdvice] = useState(''); 
+  const [advice, setAdvice] = useState('Hi! Tap to the button below for advice'); 
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    fetchAdvice();
-  }, []);
+  const ADVICE_ID_MIN = 1;
+  const ADVICE_ID_MAX = 217;
+  
+  const getRandomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  // We need to validate id of advices, because 22,48,67 are bad values in API
+  const validateRandomMumber = number => [22,48,67].includes(number) ? 1 : number; 
 
   const fetchAdvice = () => {
     setIsLoading(true);
-    axios.get('https://api.adviceslip.com/advice')
-    .then((response) => {
-      const { advice } = response.data.slip;
-      setAdvice(advice);
+
+    const randomNumber = getRandomNumber(ADVICE_ID_MIN, ADVICE_ID_MAX);
+    const adviceId = validateRandomMumber(randomNumber);
+
+    fetch(`https://api.adviceslip.com/advice/${adviceId}`)
+    .then(res => res.text())
+    .then(text => JSON.parse(`${text}}`))
+    .then(obj => {
+      const {advice} = obj.slip;
+      setAdvice(advice)   
       setIsLoading(false);
     })
   }
+
+  const btnClasses = `waves-effect waves-light btn-small ${isLoading && 'btn disabled'}`;
+
   return(
     <div className="app">
       <div className="card">
-        {isLoading
-        ? <Loader />
-        : (
-          <>
-            <h1 className="heading">{advice}</h1>
-            <button className="button" onClick={fetchAdvice}>
-              <span>GIVE ME ADVICE!</span>
-            </button>
-          </>
-        )}
+        <h1 className="heading">{advice}</h1>
+        <a href="/#" className={btnClasses} onClick={fetchAdvice}>Give me advice</a>
       </div>
     </div>
   )
