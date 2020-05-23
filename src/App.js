642da890ac5018d750748e1091d8a8470afdb81e
Kirill Mager <kiruha24@mail.ru>
2020-05-23 03:41:33 +0300

    feat: add ability to get absolutely random advices

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
 
+// API has problem with 3 id's values:
+const badIds = [22,48,67];
+const showedAdvices = new Set(badIds);
+
 function App() {
   const [advice, setAdvice] = useState(''); 
   const [isLoading, setIsLoading] = useState(false);
@@ -9,23 +13,16 @@ function App() {
   const ADVICE_ID_MAX = 217;
 
   const btnClasses = `waves-effect waves-light btn-small ${isLoading && 'btn disabled'}`;
-  
+
   const getRandomNumber = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  // We need to validate id of advices, because 22,48,67 are bad values in API
-  const validateRandomMumber = number => [22,48,67].includes(number) ? 1 : number; 
-
-  const fetchAdvice = () => {
+  const fetchAdvice = (id) => {
     setIsLoading(true);
-
-    const randomNumber = getRandomNumber(ADVICE_ID_MIN, ADVICE_ID_MAX);
-    const adviceId = validateRandomMumber(randomNumber);
-
-    fetch(`https://api.adviceslip.com/advice/${adviceId}`)
+    fetch(`https://api.adviceslip.com/advice/${id}`)
     .then(res => res.text())
     .then(text => JSON.parse(`${text}}`))
     .then(obj => {
@@ -35,15 +32,33 @@ function App() {
     })
   }
 
+  const getNewAdvice = () => {
+    if (showedAdvices.size < ADVICE_ID_MAX) {
+      const randomNumber = getRandomNumber(ADVICE_ID_MIN, ADVICE_ID_MAX);
+      if (!showedAdvices.has(randomNumber)) {
+        showedAdvices.add(randomNumber);
+        fetchAdvice(randomNumber);
+      } else {
+        showedAdvices.add(randomNumber);
+        if (showedAdvices.size === ADVICE_ID_MAX - 1){
+          setAdvice('WOW! You are awesome! You got all advices for today! Come back tomorrow!');
+          setIsLoading(true);
+        } else {
+          getNewAdvice();
+        }
+      }
+    }
+  };
+
   useState(() => {
-    fetchAdvice();
-  }, [])
+    getNewAdvice();
+  }, []);
 
   return(
     <div className="app">
       <div className="card">
-        <h1 className="heading">{isLoading ? '' : advice}</h1>
-        <a href="/#" className={btnClasses} onClick={fetchAdvice}>Give me advice</a>
+        <h1 className="heading">{advice}</h1>
+        <a href="/#" className={btnClasses} onClick={getNewAdvice}>Give me advice</a>
       </div>
     </div>
   )
