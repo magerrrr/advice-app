50d03e70205529b3e787a73cf5bd0af09669e53f
Kirill Mager <kiruha24@mail.ru>
2020-05-24 23:29:32 +0300

    refactor: add hook for showed advices

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
 
-// API has problem with 3 id's values:
-const badIds = [22,48,67];
-const showedAdvices = new Set(badIds);
-
 function App() {
-  const [advice, setAdvice] = useState(''); 
-  const [isLoading, setIsLoading] = useState(false);
-
   const ADVICE_ID_MIN = 1;
   const ADVICE_ID_MAX = 217;
-
+  const BAD_IDs = [22,48,67];
+  
+  const [advice, setAdvice] = useState(''); 
+  const [isLoading, setIsLoading] = useState(false);
+  const [showedAdvices, setShowedAdvices] = useState(new Set(BAD_IDs));
   const btnClasses = `waves-effect waves-light btn-small ${isLoading && 'btn disabled'}`;
+  
+  const addAdvice = id => setShowedAdvices( showedAdvices => new Set([...showedAdvices, id]));
 
   const getRandomNumber = (min, max) => {
     min = Math.ceil(min);
@@ -36,10 +35,10 @@ function App() {
     if (showedAdvices.size < ADVICE_ID_MAX) {
       const randomNumber = getRandomNumber(ADVICE_ID_MIN, ADVICE_ID_MAX);
       if (!showedAdvices.has(randomNumber)) {
-        showedAdvices.add(randomNumber);
+        addAdvice(randomNumber)
         fetchAdvice(randomNumber);
       } else {
-        showedAdvices.add(randomNumber);
+        addAdvice(randomNumber);
         if (showedAdvices.size === ADVICE_ID_MAX - 1){
           setAdvice('WOW! You are awesome! You got all advices for today! Come back tomorrow!');
           setIsLoading(true);
